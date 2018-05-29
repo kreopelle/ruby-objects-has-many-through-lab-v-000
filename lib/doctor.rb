6374c6d7707ae93d6ad5ce3a8e38574ea09e9b65
Kayla Reopelle <kreopelle@gmail.com>
2018-05-29 21:16:29 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,8 +11,8 @@ class Doctor
     @@all
   end
 
-  def new_appointment(date, patient)
-    appointment = Appointment.new(date, patient, self)
+  def new_appointment(patient, date)
+    appointment = Appointment.new(patient, self, date)
   end
 
   def appointments
