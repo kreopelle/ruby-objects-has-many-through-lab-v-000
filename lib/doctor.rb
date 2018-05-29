f3fd1c00476150ea6a4f8b7e59a490caef7a26a5
Kayla Reopelle <kreopelle@gmail.com>
2018-05-29 21:07:54 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,6 +13,12 @@ class Doctor
 
   def new_appointment(date, patient)
     appointment = Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
   end 
 
 end
