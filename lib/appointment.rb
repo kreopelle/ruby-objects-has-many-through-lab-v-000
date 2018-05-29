a2bde3e14274ce061e70db83e914a91e9365e5f6
Kayla Reopelle <kreopelle@gmail.com>
2018-05-29 21:05:01 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,8 +5,12 @@ class Appointment
   def initialize(date, patient, doctor)
     @date = date
     @patient = patient
-    @doctor = doctor 
-    @@all << self 
-  end 
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
